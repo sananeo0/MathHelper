64566f064706c51927cd563976367b0694c88aff
sananeo0 <sananeo@code.edu.az>
2023-08-16 15:10:26 +0400

    end

---
+++ MathHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathHelper
+{
+    public class MathHelper
+    {
+        public int PrintValue(int a, int b)
+        {
+            return a + b;
+        }
+        
+        public void PrintValue(string a, string b)
+        {
+            Console.WriteLine(a + b); 
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            
+            var mathhelper = new MathHelper();
+            mathhelper.PrintValue("senan", "orucov");
         }
     }
 }
